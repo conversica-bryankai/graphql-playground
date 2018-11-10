ff2e69e8f68b7ae51f54e5cd88b4de4342a410d2
conversica-bryankai <bryan.kai@conversica.com>
2018-11-09 17:13:40 -0800

    Created React App in client directory

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class BookList extends Component {
+  render() {
+    return (
+      <ul id="book-list">
+        <li>Book Name</li>
+      </ul>
+    );
+  }
+}
+
+export default BookList;
