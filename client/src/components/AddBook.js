376d9e19c0af80550c93da26f7226c422a408479
conversica-bryankai <bryan.kai@conversica.com>
2018-12-07 16:57:52 -0800

    New books can be added and are displayed automatically

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
-import { gql } from "apollo-boost";
-import { graphql } from "react-apollo";
+import { graphql, compose } from "react-apollo";
 import { Button, Form } from "semantic-ui-react";
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
+import {
+  getBooksQuery,
+  getAuthorsQuery,
+  addBookMutation
+} from "../queries/queries";
 
 class AddBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      genre: "",
+      authorId: ""
+    };
+  }
+
   displayAuthors() {
-    const data = this.props.data;
+    const data = this.props.getAuthorsQuery;
     if (data.loading) {
       return [
         {
@@ -28,33 +32,57 @@ class AddBook extends Component {
         return {
           key: author.id,
           text: author.name,
-          value: author.name
+          value: author.id
         };
       });
     }
   }
 
+  handleSubmitForm(e) {
+    e.preventDefault();
+    this.props.addBookMutation({
+      variables: {
+        name: this.state.name,
+        genre: this.state.genre,
+        authorId: this.state.authorId
+      },
+      refetchQueries: [{ query: getBooksQuery }]
+    });
+  }
+
   render() {
     return (
-      <Form id="add-book">
+      <Form id="add-book" onSubmit={this.handleSubmitForm.bind(this)}>
         <Form.Field>
           <label>Book Name</label>
-          <input placeholder="Book Name" />
+          <input
+            placeholder="Book Name"
+            onChange={e => this.setState({ name: e.target.value })}
+          />
         </Form.Field>
         <Form.Field>
           <label>Genre</label>
-          <input placeholder="Genre" />
+          <input
+            placeholder="Genre"
+            onChange={e => this.setState({ genre: e.target.value })}
+          />
         </Form.Field>
         <Form.Select
           fluid
           label="Author"
           placeholder="Author"
           options={this.displayAuthors()}
+          onChange={(e, d) => {
+            this.setState({ authorId: d.value });
+          }}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit">Add Book</Button>
       </Form>
     );
   }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+  graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
